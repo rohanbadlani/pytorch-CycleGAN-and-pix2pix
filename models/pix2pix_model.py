e594145383b983efb0e6c303c7a36a17a0f13e50
Jennifer Villa <jvilla2013@gmail.com>
2019-05-30 13:19:22 -0700

    Had to compute the L1 loss even if not using it due to some downstream applications.

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -150,8 +150,9 @@ class Pix2PixModel(BaseModel):
         
         self.loss_G = self.loss_G_GAN
         
+        self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
+        
         if "L1" in self.opt.loss_type:
-            self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
             self.loss_G += self.loss_G_L1
         
         if "contextual" in self.opt.loss_type:
